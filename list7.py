4ddc969cddc9f3c1cf9b6c396a41da7fd2beab3d
dhevanthareza <dhevanmuhamad@gmail.com>
2019-12-01 22:24:27 +0700

    fix list7

--- list7.py
+++ list7.py
@@ -4,7 +4,10 @@ NIM         : A11.2019.12293
 KELOMPOK    : A11.4118
 """
 numbers = input("Masukan Angka pisahkan dengan spasi = ").split(" ")
-for i in numbers[::-1]:
-    if(int(i) % 2 != 0):
-        print("{} terdapat pada index ke-{}".format(i, numbers.index(i)))
-        break
+ganjilNumber = 0
+index = 0
+for ind, number in enumerate(numbers):
+    if(int(number) % 2 != 0):
+        ganjilNumber = number
+        index = ind
+print("{} terdapat pada index ke-{}".format(ganjilNumber, index))        
